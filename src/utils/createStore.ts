90c2eb919d823ea8a9506fc79a36249764f33256
evanryuu <evanryuu@outlook.com>
2023-12-28 18:16:24 +0800

    fix: skip typecheck persistStore with any

--- src/utils/createStore.ts
+++ src/utils/createStore.ts
@@ -1,32 +1,32 @@
-import { persist } from "zustand/middleware";
-import { create } from "zustand";
-import { useEffect } from "react";
+import { persist } from 'zustand/middleware'
+import { create } from 'zustand'
+import { useEffect } from 'react'
 
-export function createPersistedStore(createStore, persistOptions) {
-  const usePersistedStore = create(persist(createStore, persistOptions));
+export function createPersistedStore(createStore: any, persistOptions: any) {
+  const usePersistedStore = create(persist(createStore, persistOptions))
 
-  const useMounted = create(() => false);
+  const useMounted = create(() => false)
 
   const initialState = createStore(
     () => {},
-    () => createStore(() => {})
-  );
+    () => createStore(() => {}),
+  )
 
-  const useStore = (selector) => {
-    const store = usePersistedStore(selector);
-    const mounted = useMounted();
+  const useStore = (selector: any) => {
+    const store = usePersistedStore(selector)
+    const mounted = useMounted()
 
     useEffect(() => {
       if (!mounted) {
-        useMounted.setState(true);
+        useMounted.setState(true)
       }
-    }, []);
+    }, [])
 
-    return mounted ? store : selector(initialState);
-  };
+    return mounted ? store : selector(initialState)
+  }
 
-  useStore.getState = usePersistedStore.getState;
-  useStore.setState = usePersistedStore.setState;
+  useStore.getState = usePersistedStore.getState
+  useStore.setState = usePersistedStore.setState
 
-  return useStore;
+  return useStore
 }
