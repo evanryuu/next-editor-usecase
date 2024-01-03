732dd636d0028c7b64e2951bfd328159d0401c3d
evanryuu <evanryuu@outlook.com>
2024-01-03 18:21:34 +0800

    chore: add organization to see if gpt works

--- src/app/api/generate/route.ts
+++ src/app/api/generate/route.ts
@@ -5,7 +5,8 @@ import { OpenAIStream, StreamingTextResponse } from 'ai'
 
 // Create an OpenAI API client (that's edge friendly!)
 const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY || '',
+  apiKey: process.env.OPENAI_API_KEY,
+  organization: process.env.OPENAI_ORGANIZATION,
 })
 
 // IMPORTANT! Set the runtime to edge: https://vercel.com/docs/functions/edge-functions/edge-runtime
